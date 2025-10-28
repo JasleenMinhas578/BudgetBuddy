33e243c7f80a4ebb5c9ba17f51d1a95a99426d3f
Masroor Rahman <80355396+MshRoor@users.noreply.github.com>
2025-10-28 18:04:16 -0230

    Worked on #28: Rectified errors of previous commit

--- src/components/Dashboard/Categories.jsx
+++ src/components/Dashboard/Categories.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { collection, addDoc, query, onSnapshot} from 'firebase/firestore';
-import { db } from '../../firebase';
+import { db } from '../../firebaseConfig';
 import { useAuth } from '../../context/AuthContext';
 import Modal from '../UI/Modal';
 import Toast from '../UI/Toast';
